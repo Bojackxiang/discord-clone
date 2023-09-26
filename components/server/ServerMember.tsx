1b26e224fe395aa16483df904006adca0879e999
Alex <alex@q-83.com>
2023-09-26 22:50:03 +1000

    update on member click

--- components/server/ServerMember.tsx
+++ components/server/ServerMember.tsx
@@ -26,8 +26,14 @@ export const ServerMember = (props: ServerMemberProps) => {
   const { member } = props;
   const params = useParams();
   const router = useRouter();
+  const {} = params;
 
   const icon = roleIconMap[member.role];
+
+  const onMemberClicked = () => {
+    const url = `/servers/${params.serverId}/conversations/${member.id}`
+    router.push(url)
+  }
 
   return (
     <button
@@ -35,6 +41,7 @@ export const ServerMember = (props: ServerMemberProps) => {
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
       )}
+      onClick={onMemberClicked}
     >
       <UserAvatar
         src={member.profile.imageUrl}
